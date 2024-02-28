d3113e9c2df420da395bdaedd5caf6d59862f576
minimum <pt_choi5246@naver.com>
2024-02-28 21:24:43 +0900

    20240228_2124 hw

---
+++ src/hw/Hw0228_3.java
@@ -0,0 +1,23 @@
+package hw;
+
+import java.util.Scanner;
+
+/*
+3번 : 2자리 숫자를 입력받아서, 입력받은 숫자를 거꾸로 1이될때까지 출력하되,
+한줄에 5개씩 출력하는 프로그램을 만드시오.
+ */
+
+public class Hw0228_3 {
+	public static void main(String[] args) {
+		
+		int su1, su2, tot;
+				
+		Scanner sc= new Scanner(System.in);
+		
+		System.out.print("두개의 숫자 중 첫번째 숫자를 입력해주세요!  ");
+		su1 = sc.nextInt();
+		
+		System.out.print("두개의 숫자 중 두번째 숫자를 입력해주세요!  ");
+		su2 = sc.nextInt();
+	}
+}
